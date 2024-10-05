954f4a8dac79c0da46db3f27c258a6d1195aa2f0
ItzMeTheAcoustism <milicamartic2023@gmail.com>
2024-10-05 11:57:09 +0200

    Nuh uh lil bro

---
+++ pajtonzadaci/main.py
@@ -0,0 +1,8 @@
+from pravogaonik import izracunaj_obim
+from pravogaonik import izracunaj_povrsinu
+
+a = int(input("Stranica a = "))
+b = int(input("Stranica b = "))
+
+izracunaj_povrsinu(a, b)
+izracunaj_obim(a, b)
---
+++ pajtonzadaci/pravogaonik.py
@@ -0,0 +1,5 @@
+def izracunaj_obim(a, b):
+    print("O = ", 2*a+2*b)
+
+def izracunaj_povrsinu(a, b):
+    print("P = ", a*b)
